0b6603288f88ff8ad337f333a276c97b4f559ab0
mohit-dexbytes <53266421+mohit-dexbytes@users.noreply.github.com>
2021-07-28 19:03:33 +0530

    Line item total key issue resolved
    Level 3 portico is throwing error 500 for commercialData->lineItems > *LineItemTotalAmt*
    As mentioned in the below doc the *LineItemTotalAmt* key should be *ItemTotalAmt*

--- src/Gateways/PorticoConnector.php
+++ src/Gateways/PorticoConnector.php
@@ -777,7 +777,7 @@ class PorticoConnector extends XmlGateway implements IPaymentGateway
                             }
 
                             if (!empty($lineItem->totalAmount)) {
-                                $linetItemNode->appendChild($xml->createElement('LineItemTotalAmt', $lineItem->totalAmount));
+                                $linetItemNode->appendChild($xml->createElement('ItemTotalAmt', $lineItem->totalAmount));
                             }
 
                             // if (!empty($lineItem->somethingsome)) {
